2d40708876bf6971555a763f4105ddcbf16be4e4
FIC <FIC@info.local>
2019-09-13 11:09:46 -0300

    Alteração no codigo

--- Aula 01/Program.cs
+++ Aula 01/Program.cs
@@ -30,7 +30,57 @@ namespace Aula_01
 
 
 
+        //variavel do tipo texto (caracteres)
+        string nomeAluno;
+
+        //quebra linha
+        Console.WriteLine();
+
+
         Console.WriteLine(num2 + num5);
+
+        
+        //Escreva em uma linha quebrando-a 
+        Console.WriteLine("Bem vindos a nossa primeira aplicação!");
+
+
+        //Continua escrevendo na mesma linha
+        Console.Write("Digite seu nome");
+        Console.Write(" seu nome: ");
+
+       
+        //captudo o dado digitando pelo usuario e salvo na variavel
+        nomeAluno = Console.ReadLine();
+
+
+        //Concatenação
+        Console.WriteLine(nomeAluno + ", Agora digite sua primeira nota ");
+
+
+        num3 = int.Parse(Console.ReadLine());
+
+
+        //Interpolação
+
+        Console.WriteLine( $"Sua primeira nota é {num3} ");
+
+
+
+        Console.WriteLine("Digite sua segunda nota");
+        num4 = int.Parse(Console.ReadLine() );
+
+
+        Console.WriteLine("Terceira nota: ");
+        num5 = int.Parse(Console.ReadLine() );
+
+
+        media = (num3 + num4 + num5) /3;
+
+        Console.WriteLine("sua media é: " + media);
+        
+
+
+
 
         }
     }

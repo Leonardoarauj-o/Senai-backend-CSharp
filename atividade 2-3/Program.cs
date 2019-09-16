da12bdebe143b740b6ebd06d73ab8cc6c35a6e0d
FIC <FIC@info.local>
2019-09-16 14:14:26 -0300

    exercicio 3-2

---
+++ atividade 2-3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace atividade_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        //deixamos declarada falsa por padrão
+        bool entrada = false;
+        int idade;
+        string retorno = "entrada não permitida";
+
+         Console.Write("digite a sua idade: ");
+         idade = int.Parse( Console.ReadLine() );
+
+        //como ja atribuimos anteriormente, mudamos o
+        //estado da variavel para verdadeira caso
+        //entre nessa condição  
+         if(idade > 17){
+             entrada = true;
+         }
+
+         if( entrada == true){
+             retorno = "entrada permitida!!!";
+
+         }else{
+             retorno = "entrada não permitida";
+         }
+
+         Console.WriteLine(retorno);
+
+
+        }
+    }
+}
